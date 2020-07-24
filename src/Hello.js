03a63a89de0292a044d5088da2cbfaf99467ef76
Shjani49 <shjani49@gmail.com>
2020-07-24 11:26:57 -0600

    Add 'Hello' component.

---
+++ src/Hello.js
@@ -0,0 +1,27 @@
+// We must import React in order to reference "React.Component" and/or other React classes/features.
+import React from 'react';
+// We extend a class to carry over all of its properties/methods, and we can add our own for our new class.
+class Hello extends React.Component {
+    /**
+     * render
+     * @Summary An expected method for React components.
+     * @return (JSX) Output object/string for when this component is referenced.
+     */
+    render()
+    {
+        /**
+         * We can only return one object at a time.
+         * This means we must have a wrapper JSX 'element' , or if we don't want a wrapper...
+         * we can use <></> ( a blank 'element' ) to get around that!
+         *  */ 
+        return ( 
+            <> 
+                <h2>My First Component</h2>
+                <p>Hello, World!</p>
+            </>
+        )
+    }
+}
+
+// In order for Hello to be accessible via import in other files, we have to have an export in this file.
+export default Hello;
